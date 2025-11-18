0d68654ac8954ff79107b85e555276c09e2c1b45
gankris123 <ganiemodesto300@gmail.com>
2025-11-18 10:05:26 +0800

    Added sum computation

--- math.js
+++ math.js
@@ -23,3 +23,26 @@ diff(2,3);
 prod(2,3);
 quo(2,3);
 */
+function add(num1,num2){
+    return num1 + num2;
+}
+
+
+function subtract(num1,num2){
+    return num1 - num2;
+}
+
+
+function multiply(num1,num2){
+    return num1 * num2;
+}
+
+
+function divide(num1,num2){
+    return num1 / num2;
+}
+
+console.log(add(2,3));
+console.log(subtract(2,3));
+console.log(multiply(2,3));
+console.log(divide(2,3));
